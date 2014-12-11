d09fb91d79718e511894c1623c510138ddea1dd9
Mark William Ryan <markryan1988@gmail.com>
2014-12-11 17:21:59 -0500

    Add empty Gruntfile.js

---
+++ Gruntfile.js
@@ -0,0 +1,13 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    
+  });
+
+  //grunt.loadNpmTasks('task-goes-here');
+
+  // Default task(s).
+  grunt.registerTask('default', []);
+
+};
